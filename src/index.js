28c18f36eb1c46285027a90e9449cdc94119a8e1
Maxwell O'Roark <m.d.oroark@gmail.com>
2022-11-08 06:50:52 -0600

    improve blinking cursor logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const ids = ["about", "projects", "links"];
 
 window.addEventListener("scroll", () => {
+  const d = document.documentElement;
+  const offset = d.scrollTop + window.innerHeight;
+  const height = d.offsetHeight;
+
   ids.forEach((id) => {
     const header = document.getElementById(id);
     const distanceToTop = header.getBoundingClientRect().top;
@@ -12,4 +16,14 @@ window.addEventListener("scroll", () => {
       cursor.classList.remove("active-cursor");
     }
   });
+
+  // if at the bottom of the page just apply blinking cursor to links section
+  if (offset >= height) {
+    console.log("bottom of page");
+    const cursor = document.getElementById(`links-cursor`);
+    cursor.classList.add("active-cursor");
+  } else {
+    const cursor = document.getElementById(`links-cursor`);
+    cursor.classList.remove("active-cursor");
+  }
 });
